d6299fbc4ed4e8a2f16bf2d150b91bf865a23ad0
unknown <laz51@pitt.edu>
2021-10-06 01:25:17 -0400

    Fixing random shit

--- nbafrontend/tailwind.config.js
+++ nbafrontend/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    purge: ["./public/index.html"],
+    purge: ["./src/components/**/*.{js,jsx,ts,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
